fb9663b93bfe4f24829824904ca1fb8d23cf23fe
semmozhiyan-dev <semmozhi12122005@gmail.com>
2026-06-03 23:25:12 +0530

    fixed issues in index.js

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const fs = require('fs');
 fs.readFile('./files/starter.txt','utf8',(err,data)=>{
     if(err) throw err;
     console.log(data);
-}
+    }
 )
+    //exit on uncaught error
+    process.on('uncaughtException',err=>{
+        console.error('the uncaught error was identified:${err}');
+        process.exit(1);
+    })
