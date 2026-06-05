15687f2bf8dca8849e00902381e32e7ca9d4ca15
semmozhiyan-dev <semmozhi12122005@gmail.com>
2026-06-05 09:05:56 +0530

    refactor filecreation.js to include file reading and deletion functionality; add stream.js

--- filecreation.js
+++ filecreation.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 // if file exist means it return true if not false  statement will run and then if statement will run and create a folder docs
+//mkdir-> make directory
 if(! fs.existsSync('./docs')){
 fs.mkdir('./docs',(err)=>
 {
@@ -16,4 +17,42 @@ fs.writeFile('./docs/file.txt','hi sem',(err)=>{
     }
     console.log('file created and data was written');
 }
+
 )
+//these code tells to read a file which is existed
+if(! fs.existsSync('./docs/file.txt')){
+
+fs.readFile('./docs/file.txt',(err,data)=>{
+    if(err){
+        console.log(err.message);
+
+
+    }else{
+        console.log(data.toString());
+    }
+}
+
+)}
+//to delete a file  in a folder
+if(fs.existsSync('./docs/file.txt')){
+    fs.unlink('./docs/file.txt',(err)=>{
+        if(err){
+            console.log(err.message);
+        }
+        else{
+            console.log('file deleted');
+        }
+    }
+)
+}
+//to delete a folder
+if(fs.existsSync('./docs')){
+fs.rmdir('./docs',(err)=>{
+    if(err){
+        console.log(err.message);
+    }else{
+        console.log('folder deleted');
+    }
+}
+)
+}
